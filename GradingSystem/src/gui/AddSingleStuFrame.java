d8c837657f49a0eea3c0b25deed1dbb60a4289e7
yujchen915 <chenyujingYW@163.com>
2019-12-06 18:51:05 -0500

    update

--- GradingSystem/src/gui/AddSingleStuFrame.java
+++ GradingSystem/src/gui/AddSingleStuFrame.java
@@ -2,16 +2,19 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import objects.Student;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.event.ActionEvent;
 
 public class AddSingleStuFrame extends JFrame implements FrameActions{
@@ -24,59 +27,96 @@ public class AddSingleStuFrame extends JFrame implements FrameActions{
 	private JLabel lblStudentId;	
 	private JLabel lblStudentEmail;	
 	private JButton btnAdd;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private List<Student> students=new ArrayList<Student>();
 
 	/**
 	 * Create the frame.
 	 */
-	public AddSingleStuFrame() {
+	public AddSingleStuFrame(List<Student> students) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 355, 371);
+		setBounds(100, 100, 465, 524);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		init();
 		addActions();
+		this.students=students;
 	}
 	
 	public void init(){
-		lblStudentName = new JLabel("Student Name:");
+		lblStudentName = new JLabel("First Name:");
 		lblStudentName.setBounds(51, 46, 108, 15);
 		contentPane.add(lblStudentName);
 		
 		textField = new JTextField();
-		textField.setBounds(51, 71, 232, 21);
+		textField.setBounds(51, 71, 335, 21);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		lblStudentId = new JLabel("Student ID:");
-		lblStudentId.setBounds(51, 113, 76, 15);
+		lblStudentId = new JLabel("ID:");
+		lblStudentId.setBounds(52, 195, 76, 15);
 		contentPane.add(lblStudentId);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(51, 138, 232, 21);
+		textField_1.setBounds(51, 223, 335, 21);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
-		lblStudentEmail = new JLabel("Student Email:");
-		lblStudentEmail.setBounds(51, 181, 108, 15);
+		lblStudentEmail = new JLabel("Email:");
+		lblStudentEmail.setBounds(51, 342, 108, 15);
 		contentPane.add(lblStudentEmail);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(51, 206, 232, 21);
+		textField_2.setBounds(51, 297, 335, 21);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		btnAdd = new JButton("Add");
-		btnAdd.setBounds(213, 288, 93, 23);
+		btnAdd.setBounds(293, 419, 93, 23);
 		contentPane.add(btnAdd);
+		
+		JLabel lblStudentGender = new JLabel("Gender:");
+		lblStudentGender.setBounds(51, 268, 124, 16);
+		contentPane.add(lblStudentGender);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(51, 370, 335, 22);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		lblLastName.setBounds(51, 122, 108, 16);
+		contentPane.add(lblLastName);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(51, 151, 335, 22);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
 	}
 	
 	public void addActions(){
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				String fname=textField.getText();
+				String lname=textField_4.getText();
+				String ID=textField_1.getText();
+				String gender=textField_2.getText();
+				String email=textField_3.getText();
+				Student singleStu=new Student(fname,lname,gender,ID,email);
+				students.add(singleStu);
 			}
 		});
+	}
+	
+	public void setStudents(List<Student> students) {
+		this.students=students;
+	}
+	
+	public List<Student> getStudents(){
+		return this.students;
 	}
 	
 	public void alert(String message){
@@ -94,5 +134,4 @@ public class AddSingleStuFrame extends JFrame implements FrameActions{
 	public void openPrevious() {
 		dispose();
 	}
-
 }
