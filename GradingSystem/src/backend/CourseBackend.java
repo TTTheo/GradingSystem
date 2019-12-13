0dc8c4daf06e7f800de4f9c56e3146248ac9471f
David Shen <dshen1@bu.edu>
2019-12-13 14:40:15 -0500

    Integrates category and part into CourseBackend

--- GradingSystem/src/backend/CourseBackend.java
+++ GradingSystem/src/backend/CourseBackend.java
@@ -12,12 +12,13 @@ import dao.*;
 // Logic for everything specific to one course, including
 // categories, parts, students, grades
 public class CourseBackend {
-    private CourseDao cd = new CourseDao() ;
-    private CategoryBackend catb = new CategoryBackend() ;
+	private CourseDao courseDao = new CourseDao();
+	private CategoryDao categoryDao = new CategoryDao();
+	private PartDao partDao = new PartDao();
 
-    Course currentCourse;
+	Course currentCourse;
 
-	public CourseBackend () {
+	public CourseBackend() {
 		currentCourse = null;
 	}
 
@@ -28,75 +29,112 @@ public class CourseBackend {
 	public void setCurrentCourse(Course c) {
 		currentCourse = c;
 	}
-    
-    public ArrayList<Course> getAllCourse(Semester semester) {
-    	ArrayList<Course> courses = null;
-    	try {
-			courses = cd.getAll(semester.getTerm(), semester.getYear()) ;
-			if(courses != null) {
-				for(Course course : courses) {
-					ArrayList<Category> cats = catb.getCategories(course.getCourseid()) ;
+
+	// Course methods
+
+	// Gets every single course from the database in semester
+	public ArrayList<Course> getAllCourse(Semester semester) {
+		ArrayList<Course> courses = null;
+		try {
+			courses = courseDao.getAll(semester.getTerm(), semester.getYear());
+			if (courses != null) {
+				for (Course course : courses) {
+					ArrayList<Category> cats = getCategories(course);
 					course.setCategories(cats);
 					course.setCategoryCount(cats.size());
 				}
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	return courses ;
-    }
-    
-    public Course getCourse(String courseid) {
-    	ArrayList<Course> courses = null ;
-    	Course course = null ;
-    	try {
-    		courses = cd.getCourse(courseid) ;
-    		if(courses == null) return null ;
-    		course = courses.get(0) ;
-    		ArrayList<Category> cats = new ArrayList<>() ;
-    		cats = catb.getCategories(courseid) ;
-    		course.setCategories(cats);
-    		course.setCategoryCount(cats.size());
-    	}catch(SQLException e){
-    		e.printStackTrace();
-    	}
-    	return course ;
-    }
-    
-    public boolean addCourse(Course course) {
-    	if(course == null) return false ;
-    	try {
-    		cd.insert(course);
-    		ArrayList<Category> cats = course.getCategories() ;
-    		if(cats != null) {
-    			for(Category cat : cats) {
-    				catb.addCategory(cat) ;
-    			}
-    		}
-    	}catch(SQLException e) {
-    		e.printStackTrace();
-    		return false ;
-    	}
-    	return true ;
-    }
-    
-    public boolean updateCourse(Course course) {
-    	if(course == null) return false ;
-    	try {
-    		cd.update(course);
-    		ArrayList<Category> cats = course.getCategories() ;
-    		if(cats != null) {
-    			for(Category cat : cats) {
-    				catb.updateCategory(cat) ;
-    			}
-    		}
-    	}catch(SQLException e) {
-    		e.printStackTrace();
-    		return false ;
-    	}
-    	return true ;
-    }
+		return courses;
+	}
+
+	// Get a course by it's course id (eg. "cs591")
+	// Populates the course's categories (and parts) as well
+	public Course getCourse(String courseid) {
+		ArrayList<Course> courses = null;
+		Course course = null;
+		try {
+			courses = courseDao.getCourse(courseid);
+			if (courses == null) return null;
+			course = courses.get(0);
+			ArrayList<Category> cats = getCategories(course);
+			course.setCategories(cats);
+			course.setCategoryCount(cats.size());  // TODO remove this useless field
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return course;
+	}
+
+	public boolean addCourse(Course course) {
+		if (course == null) return false;
+		try {
+			courseDao.insert(course);
+			ArrayList<Category> cats = course.getCategories();
+			if (cats != null) {
+				for (Category cat : cats) {
+					addCategory(cat);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean updateCourse(Course course) {
+		if (course == null) return false;
+		try {
+			courseDao.update(course);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 
 	/* Category Backend Methods */
+
+	// Get all categories for a course
+	public ArrayList<Category> getCategories(Course c) throws SQLException {
+		ArrayList<Category> categories = new ArrayList<>();
+		categories = categoryDao.getAll(c.getCourseid());
+		if (categories.size() == 0) return null;
+		for (Category cat : categories) {
+			ArrayList<Part> parts = getParts(cat);
+			cat.setPartList(parts);
+		}
+		return categories;
+	}
+
+	public boolean addCategory(Category category) throws SQLException {
+		categoryDao.insert(category);
+		ArrayList<Part> parts = category.getPartList();
+		if (parts != null && parts.size() != 0) {
+			for (Part part : parts) {
+				addPart(part);
+			}
+		}
+		return true;
+	}
+
+	/* Part backend methods */
+
+	// Get all parts for a category
+	public ArrayList<Part> getParts(Category c) throws SQLException {
+		ArrayList<Part> parts = partDao.getAll(c.getCid());
+		if (parts.size() == 0) return null;
+		return parts;
+	}
+
+	public void addPart(Part part) throws SQLException {
+		partDao.insert(part);
+	}
+
+	/* Student backend methods */
+
+	/* Grade backend methods */
 }
